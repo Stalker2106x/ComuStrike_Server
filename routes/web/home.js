35af74d923abf23ddb1ad7008f4e60bf140d7b11
stalker2106 <stalker2106x@gmail.com>
2023-08-30 19:23:24 +0200

    fixed web routes on build

--- routes/web/home.js
+++ routes/web/home.js
@@ -3,11 +3,11 @@ const fs = require('fs')
 const path = require('path')
 
 module.exports = {
-  handler: (app, req, res, next) => {
-    const templatePath = path.join(__dirname, '../../web', 'home.html')
-    const css = fs.readFileSync(path.join(__dirname, '../../web', 'web.css'), 'utf-8')
+  handler: async (app, req, res, next) => {
+    const template = await fs.readFileSync(path.join(__dirname, '../../web', 'home.html'), 'utf-8')
+    const css = await fs.readFileSync(path.join(__dirname, '../../web', 'web.css'), 'utf-8')
     const version = `Server v${app.config.serverVersion}  -  Game v${app.config.gameVersion}`
-    const page = nunjucks.configure({ autoescape: false }).render(templatePath, { css, serverList: app.serverList, version })
+    const page = nunjucks.configure({ autoescape: false }).renderString(template, { css, serverList: app.serverList, version })
     res.set('Content-Type', 'text/html; charset=utf-8')
     res.send(Buffer.from(page))
   }
--- routes/web/register.js
+++ routes/web/register.js
@@ -4,10 +4,10 @@ const path = require('path')
 
 module.exports = {
   handler: async (app, req, res, next) => {
-    const templatePath = path.join(__dirname, '../../web', 'register.html')
+    const template = await fs.readFileSync(path.join(__dirname, '../../web', 'register.html'), 'utf-8')
     const css = await fs.readFileSync(path.join(__dirname, '../../web', 'web.css'), 'utf-8')
     const registerScript = await fs.readFileSync(path.join(__dirname, '../../web/scripts', 'register.js'), 'utf-8')
-    const page = nunjucks.configure({ autoescape: false }).render(templatePath, { css, registerScript })
+    const page = nunjucks.configure({ autoescape: false }).renderString(template, { css, registerScript })
     res.set('Content-Type', 'text/html; charset=utf-8')
     res.send(Buffer.from(page))
   }
